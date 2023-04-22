b304b28bbd765ebe5f1f8aa7680692ab6a8f9e75
nateainsworth <74594366+nateainsworth@users.noreply.github.com>
2023-04-22 23:52:23 +0100

    added web server controls and joystick controls

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,110 @@
-#include <Arduino.h>
-#include <Servo.h> 
+#include <ESP8266WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include <Servo.h>
+#include "LittleFS.h"
+
+#define XSTR(x) #x
+#define STR(x) XSTR(x)
+
+//sets wifi up from env
+const char* ssid = STR(SSID);
+const char* password = STR(PASS);
+
+AsyncWebServer server(80);
+
+String horizontalString = String(20);
 
 Servo horizontal, vertical, tilt;
-void setup() 
-{ 
+
+int joystick;
+
+int joystickX = A0;
+int joystickY = D1;
+int joystickSwitch = D5;
+int joystickState = 0;
+
+String processor(const String& var){
+  Serial.println(var);
+  // retrieve state value 
+  //TODO CHANGE TO HORIZONTAL
+  if(var == "STATE"){
+    Serial.print(horizontalString);
+    return horizontalString;
+  }
+  return String();
+}
+
+void setup() {
   Serial.begin(115200);
-  vertical.attach(D6, 500,2050);  // attaches the servo on pin 11 to the middle object
-  horizontal.attach(D7);
+  pinMode(joystickSwitch, INPUT_PULLUP);
+  horizontal.attach(D6,500,2050);
+  vertical.attach(D7);
   tilt.attach(D8);
-} 
- 
-void loop() 
-{ 
-  vertical.write(0);
-  horizontal.write(0);
-  //middle.write(20);
-  delay(1000); 
-  horizontal.write(180);
-  vertical.write(180);
-  delay(3000); // doesn't constantly update the servos which can fry them
-} 
+
+
+  LittleFS.begin();
+/*
+  TODO FIND AN ALTERNATIVE FOR ERROR REPORTING IN LITTLEFS
+  if(!LittleFS.begin(true)){
+    Serial.println("LittleFs Failed to mount");
+    return;
+  }*/
+
+
+
+  WiFi.hostname("Gimbal");
+  Serial.println("Connecting to wifi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.print("IP address: 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.print("/' ");
+  Serial.println("");
+
+  // Load web sever pages and css
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/index.html", String(), false, processor);
+  });
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/style.css", "text/css");
+  });
+
+  //catch web server endpoint for horizontal servo
+  server.on("/horizontal", HTTP_GET, [](AsyncWebServerRequest *request){
+    // check for slider value
+    if(request->hasParam("value")){
+      AsyncWebParameter* p = request->getParam("value");
+      Serial.println(p->value());
+      horizontal.write(p->value().toInt());
+      horizontalString = p->value();
+    }
+    // update STATE
+    request->send(LittleFS, "/index.html", String(), false, processor);
+  });
+
+  server.begin();
+}
+
+void loop(){
+/*
+  TODO ONLY TURN ON WHEN BUTTON IS PRESSED OTHERWISE USE WEBSITE VERSION MAYBE
+  joystick = analogRead(joystickX);
+  joystick = map (joystick, 0,1023,0,180);
+  // tell servo.
+  horizontal.write(joystick);
+
+  joystick = analogRead(joystickY);
+  joystick = map(joystick, 0,1023,0,180);
+  vertical.write(joystick);
+
+  joystickState = digitalRead(joystickSwitch);
+
+  delay(100);
+  */
+}
